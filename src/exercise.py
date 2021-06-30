964442d088c0d2bd94d87074ea4f7847bf56b36a
jamesgildea92@protonmail.com <jamesgildea92@protonmail.com>
2021-06-30 11:46:44 +0100

    Attempt 1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,6 @@
 def main():
-    #write your code below this line
-
+    print("Once upon a time")#write your code below this line
+    print("there was")
+    print("a program")
 if __name__ == '__main__':
     main()
